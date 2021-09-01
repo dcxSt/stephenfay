947aee935fea5d566632b6a4896d37f669298583
Stephen Fay <dcxstephen@live.co.uk>
2021-09-01 13:13:17 +0200

    hokku content update

--- src/pages/senryu.js
+++ src/pages/senryu.js
@@ -30,8 +30,6 @@ export default function Hokku() {
                         </div>)
                     }
             </div>
-					<script defer src="https://cdn.commento.io/js/commento.js"></script>
-					<div id="commento"></div>
         </div>
     );
 }
