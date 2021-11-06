7e09f56b8606e17d1fca5a802d0fee239b77c78f
Stephen Fay <dcxstephen@live.co.uk>
2021-11-06 01:21:53 -0700

    polished some things up, more self concious front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,34 +7,27 @@ export default function Home() {
 				<Layout>
 						<h1>Stephen Fay</h1>
             <pquote style={{ marginTop:'-16px' }}>
-                    <b>Eye color:</b> grey,<br/>
-                    <b>Language of choice:</b> Python, <br/>
-                    <b>Favourite math:</b> stoke's theorem, the isomorphism theorems, <br/> 
-                    <b>Favourite ensemble:</b> the canonical ensemble,<br/> 
-                    <b>Favourite distrubition:</b> Gibbs, <br/> 
-                    <b>Favourite physics (theory):</b> Classical physics, <br/> 
-                    <b>Longest breath holding record:</b> 3m + epsilon seconds, <br/> 
-                    <b>Unexpected talent:</b> I play the Cello, <br/> 
-                    <b>Text editor of choice:</b> vim, vscode with vim keymappings, <br/> 
-                    <b>Other hobbies:</b> Jiu-Jitsu, Judo, biking, basking in the sun.<br/>
-                    <br/>Currently I'm working on digital signal processing techniques for radio telescopes.  
+                    <b>Welcome to my website!</b>
 										<br/><br/>
 
-										<b>Little Fun things I built</b> <br/>
+										<b>Fun things</b> <br/>
 										<a href="https://github.com/dcxSt/mcgill_physics_hackathon2020">Snowflake groth cellular automaton</a>, <br/>
 										<a href="https://atom-war.dcxst.repl.co/">Atom Wars</a> - a cookie clicker game built on the Rinkeby Etherium test-net, <br/>
 										<a href="https://github.com/dcxSt/lipschitz">H-Trees</a> - my first hackathon, <br/>
-										<a href="https://github.com/dcxSt/alien_wiki_chrome_extension">Alien Wikipedia</a> - a chrome extension that makes reading wikipedia more entertaining
+										<a href="https://github.com/dcxSt/alien_wiki_chrome_extension">Alien Wikipedia</a> - a chrome extension that makes reading wikipedia more entertaining, <br/>
+                    <a href="https://github.com/dcxSt/attractors/blob/master/README.md">Strange Attractor orbits</a> - some really pretty art!
 										
 										<br/><br/>
 										<b>Homies</b> <br/>
 										<a href="https://marleyx.com" styel={{ unerline:"none" }}>Marley</a>, 
 										<a href="https://marcelgoh.ca/">Marcel</a>,
 										<a href="https://rosieyzh.github.io/" style={{ underline:"none" }}>Rosie</a>, 
+                    <a href="http://luc.devroye.org/">Luc</a>,
 										<br/><br/>
 										<b>Self</b> <br/>
 										<a href="https://curius.app/stephen-fay">curious profile</a>, 
-										<a href="https://projecteuler.net/profile/dcxStephen.png">project euler profile</a>
+										<a href="https://projecteuler.net/profile/dcxStephen.png">project euler profile</a>, 
+                    <br/><br/>send me an email: dcxstephen@gmail.com
                 </pquote>
 								<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
 						</Layout>
