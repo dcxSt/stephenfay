ec4826efa2c157f3bef06fb7f7a8c8c4bef4f696
Stephen Fay <dcxstephen@live.co.uk>
2022-01-31 00:08:53 -0500

    bukowski quote

--- src/pages/quotes.js
+++ src/pages/quotes.js
@@ -6,6 +6,10 @@ export default function Quotes() {
 			<Layout>
 				<h1>Nuggets of gold</h1>
         <pquote>
+          Somebody at one of these places [...] asked me: 'What do you do? How do you write, create?' You don't, I told them. You don't try. That's very important: *not* to try, either for Cadillacs, creation or immortality. You wait, and if nothing happens, you wait some more. It's like a bug high on the wall. You wait for it to come to you. When it gets close enough you reach out, slap out and kill it. Or, if you like its look, you make a pet out of it.
+          <br/><br/><b>Charles Bukowski</b>
+        </pquote>
+        <pquote>
           Bears are just guys.
           <br/><br/><b>Sig</b>
         </pquote>
