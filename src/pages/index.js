65d28c84a755f9067de7a82465b30d1cf9473a1b
Stephen Fay <dcxstephen@live.co.uk>
2022-02-21 13:52:50 -0500

    added substack

--- src/pages/index.js
+++ src/pages/index.js
@@ -37,11 +37,12 @@ export default function Home() {
                     <a href="https://noahtren.com/">Noah</a>,
 										<br/><br/>
 										<b>Self</b> <br/>
-										<a href="https://curius.app/stephen-fay">curious profile</a>, 
-										Project Euler <a href="https://projecteuler.net/profile/dcxStephen.png">profile</a> and <a href="https://github.com/dcxSt/project-euler">solutions</a>, 
-                    <a href="https://www.researchgate.net/profile/Stephen-Fay">Research gate</a>,
-                    <a href="https://scholar.google.com/citations?view_op=list_works&hl=en&user=eMRXnwUAAAAJ">google scholar</a>, 
-                    <a href="https://dcxst.github.io/math-tutor/">my business card and teaching philosophy</a>,
+										<a href="https://curius.app/stephen-fay">curious profile</a>, <br/>
+										Project Euler <a href="https://projecteuler.net/profile/dcxStephen.png">profile</a> and <a href="https://github.com/dcxSt/project-euler">solutions</a>, <br/>
+                    <a href="https://www.researchgate.net/profile/Stephen-Fay">Research gate</a>,<br/>
+                    <a href="https://scholar.google.com/citations?view_op=list_works&hl=en&user=eMRXnwUAAAAJ">google scholar</a>, <br/>
+                    <a href="https://dcxst.github.io/math-tutor/">my business card and teaching philosophy</a>,<br/>
+                    <a href="https://stephenfay.substack.com/">my Substack newsletter</a>
                     <br/><br/>send me an email: dcxstephen@gmail.com
                 </pquote>
 								<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
