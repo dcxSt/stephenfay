e54f94ea81634fd03200df17347e4094416b6c85
Stephen Fay <dcxstephen@live.co.uk>
2022-03-10 12:31:37 -0500

    created mathposts.js

---
+++ src/pages/mathposts.js
@@ -0,0 +1,45 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import { Link } from "gatsby"
+
+export default function MathPosts({ data }) {
+    console.log(data);
+    return (
+				<Layout>
+                <h1>Math</h1>
+                {data.allMarkdownRemark.edges.map(({ node }) => {
+                    if (node.fileAbsolutePath.includes('mathposts'))
+                        return (
+                            <div key={node.id}>
+                                <Link to={node.fields.slug} style={{ color:"inherit",textDectoration:"none",textDecorationLine:"none"}}><h4>{node.frontmatter.date} {node.frontmatter.title} <span style={{color:"#777"}}> type : {node.frontmatter.type}</span></h4></Link>
+                                {node.excerpt}
+                            </div>
+                        )
+                })}
+        </Layout>
+    );
+}
+
+export const query = graphql`
+    query {
+        allMarkdownRemark (sort: { fields: [frontmatter___date], order:DESC}) {
+            edges {
+            node {
+                id
+                excerpt
+                frontmatter {
+                    title
+                    date
+                    type
+                }
+                fileAbsolutePath
+                timeToRead
+                fields {
+                    slug
+                }
+            }
+            }
+        }
+    }
+`
