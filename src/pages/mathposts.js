52416976caf30b25f226b1a2dccfcd531da79d14
Stephen Fay <dcxstephen@live.co.uk>
2022-03-13 17:01:17 -0400

    problem 153 proj euler

--- src/pages/mathposts.js
+++ src/pages/mathposts.js
@@ -7,19 +7,24 @@ import P153 from "../assets/p153.pdf"
 export default function MathPosts({ data }) {
     console.log(data);
     return (
-				<Layout>
-                <h1>Math</h1>
-                {data.allMarkdownRemark.edges.map(({ node }) => {
-                    if (node.fileAbsolutePath.includes('mathposts'))
-                        return (
-                            <div key={node.id}>
-                                <Link to={node.fields.slug} style={{ color:"inherit",textDectoration:"none",textDecorationLine:"none"}}><h4>{node.frontmatter.date} {node.frontmatter.title} <span style={{color:"#777"}}> type : {node.frontmatter.type}</span></h4></Link>
-                                {node.excerpt}
-                            </div>
-                        )
-                })}
-                
-                <iframe src={P153} width="400" height="400" title="lala"></iframe>
+        <Layout>
+            <h1>Math</h1>
+            {data.allMarkdownRemark.edges.map(({ node }) => {
+                if (node.fileAbsolutePath.includes('mathposts'))
+                    return (
+                        <div key={node.id}>
+                            <Link to={node.fields.slug} style={{ color:"inherit",textDectoration:"none",textDecorationLine:"none"}}><h4>{node.frontmatter.date} {node.frontmatter.title} <span style={{color:"#777"}}> type : {node.frontmatter.type}</span></h4></Link>
+                            {node.excerpt}
+                        </div>
+                    )
+            })}
+
+          <div key="p153">
+            <Link to="./p153/" style={{ color:"inherit",textDectoration:"none",textDecorationLine:"none"}}>
+              <h4>Project Euler Problem 153</h4>
+            </Link>
+          </div>
+
         </Layout>
     );
 }
