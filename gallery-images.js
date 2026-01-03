565f2ff549d354fd094f98572e583c120bdd09cc
Stephen Fay <dcxstephen@gmail.com>
2026-01-03 00:29:28 +0000

    gallary update

--- gallery-images.js
+++ gallery-images.js
@@ -2,7 +2,6 @@
 // Run generate-gallery.sh to update
 
 const galleryImages = [
-    { src: 'images/EB1A2732-edit1.webp', alt: 'EB1A2732-edit1' },
     { src: 'images/EB1A2743-crop2-edit1-qhigh.webp', alt: 'EB1A2743-crop2-edit1-qhigh' },
     { src: 'images/EB1A3765-edit04.webp', alt: 'EB1A3765-edit04' },
     { src: 'images/EB1A7654-edit2.webp', alt: 'EB1A7654-edit2' },
@@ -10,8 +9,10 @@ const galleryImages = [
     { src: 'images/EB1A7931-edit2.webp', alt: 'EB1A7931-edit2' },
     { src: 'images/EB1A7951-edit2.webp', alt: 'EB1A7951-edit2' },
     { src: 'images/EB1A8134-edit1.webp', alt: 'EB1A8134-edit1' },
+    { src: 'images/EB1A8265-edit3.webp', alt: 'EB1A8265-edit3' },
     { src: 'images/EB1A8410.webp', alt: 'EB1A8410' },
     { src: 'images/EB1A8483-crop3-edit4-downsample.webp', alt: 'EB1A8483-crop3-edit4-downsample' },
     { src: 'images/EB1A8551-edit3.webp', alt: 'EB1A8551-edit3' },
-    { src: 'images/EB1A8558-crop2-edit1-qh.webp', alt: 'EB1A8558-crop2-edit1-qh' }
+    { src: 'images/EB1A8558-crop2-edit1-qh.webp', alt: 'EB1A8558-crop2-edit1-qh' },
+    { src: 'images/IMG_0465-edit1.webp', alt: 'IMG_0465-edit1' }
 ];
